f06cc4b0c1bb53e998a08916dc55ee2cbeb1b7cf
Peter Amorese <peter.amorese@gmail.com>
2024-01-20 15:29:06 -0700

    Minor fixes for fixed env bm

--- src/pareto_reinforcement_learning/scenarios/mars_rover/launch_benchmark_fixed_env.sh
+++ src/pareto_reinforcement_learning/scenarios/mars_rover/launch_benchmark_fixed_env.sh
@@ -1,5 +1,5 @@
-n_instances=150
-n_trials=100
+n_instances=2
+n_trials=2
 data_directory=fixed_env_benchmark
 conf=0.1
 
@@ -12,27 +12,27 @@ seed=11 # 6pp
 
 # Uniform 
 selector=uniform
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
 
 # TOPSIS 
 selector=topsis
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
 
 # Weights
 selector=weights
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
 
 # AIF big_var
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/aif_big_var_"$env_tag".yaml > logs/aif_big_var_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/aif_big_var_"$env_tag".yaml > logs/aif_big_var_"$env_tag".log 2>&1 &
 
 # AIF med_var
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_med_var.yaml -d $data_directory/aif_med_var_"$env_tag".yaml > logs/aif_med_var_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_med_var.yaml -d $data_directory/aif_med_var_"$env_tag".yaml > logs/aif_med_var_"$env_tag".log 2>&1 &
 
 # AIF small_var
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_small_var.yaml -d $data_directory/aif_small_var_"$env_tag".yaml > logs/aif_small_var_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_small_var.yaml -d $data_directory/aif_small_var_"$env_tag".yaml > logs/aif_small_var_"$env_tag".log 2>&1 &
 
 # AIF no_var
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_no_var.yaml -d $data_directory/aif_no_var_"$env_tag".yaml > logs/aif_no_var_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_no_var.yaml -d $data_directory/aif_no_var_"$env_tag".yaml > logs/aif_no_var_"$env_tag".log 2>&1 &
 
 
 ######################## some PP ########################
@@ -41,27 +41,27 @@ seed=7 # 32pp
 
 # Uniform 
 selector=uniform
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
 
 # TOPSIS 
 selector=topsis
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
 
 # Weights
 selector=weights
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
 
 # AIF big_var
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/aif_big_var_"$env_tag".yaml > logs/aif_big_var_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/aif_big_var_"$env_tag".yaml > logs/aif_big_var_"$env_tag".log 2>&1 &
 
 # AIF med_var
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_med_var.yaml -d $data_directory/aif_med_var_"$env_tag".yaml > logs/aif_med_var_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_med_var.yaml -d $data_directory/aif_med_var_"$env_tag".yaml > logs/aif_med_var_"$env_tag".log 2>&1 &
 
 # AIF small_var
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_small_var.yaml -d $data_directory/aif_small_var_"$env_tag".yaml > logs/aif_small_var_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_small_var.yaml -d $data_directory/aif_small_var_"$env_tag".yaml > logs/aif_small_var_"$env_tag".log 2>&1 &
 
 # AIF no_var
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_no_var.yaml -d $data_directory/aif_no_var_"$env_tag".yaml > logs/aif_no_var_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_no_var.yaml -d $data_directory/aif_no_var_"$env_tag".yaml > logs/aif_no_var_"$env_tag".log 2>&1 &
 
 ######################## many PP ########################
 env_tag="many_pp"
@@ -69,25 +69,25 @@ seed=9 # 72pp
 
 # Uniform 
 selector=uniform
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
 
 # TOPSIS 
 selector=topsis
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
 
 # Weights
 selector=weights
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector $selector -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/"$selector"_"$env_tag".yaml > logs/"$selector"_"$env_tag".log 2>&1 &
 
 # AIF big_var
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/aif_big_var_"$env_tag".yaml > logs/aif_big_var_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_big_var.yaml -d $data_directory/aif_big_var_"$env_tag".yaml > logs/aif_big_var_"$env_tag".log 2>&1 &
 
 # AIF med_var
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_med_var.yaml -d $data_directory/aif_med_var_"$env_tag".yaml > logs/aif_med_var_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_med_var.yaml -d $data_directory/aif_med_var_"$env_tag".yaml > logs/aif_med_var_"$env_tag".log 2>&1 &
 
 # AIF small_var
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_small_var.yaml -d $data_directory/aif_small_var_"$env_tag".yaml > logs/aif_small_var_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_small_var.yaml -d $data_directory/aif_small_var_"$env_tag".yaml > logs/aif_small_var_"$env_tag".log 2>&1 &
 
 # AIF no_var
-nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --no-plan-data --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_no_var.yaml -d $data_directory/aif_no_var_"$env_tag".yaml > logs/aif_no_var_"$env_tag".log 2>&1 &
+nohup stdbuf -oL ../../../../build/bin/PRLGridWorldAgentFixedEnvBenchmark -s $seed --selector aif -i $n_instances -t $n_trials --confidence $conf -f formulas.yaml -c random_config_no_var.yaml -d $data_directory/aif_no_var_"$env_tag".yaml > logs/aif_no_var_"$env_tag".log 2>&1 &
 
